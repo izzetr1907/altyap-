d6fbdaea813262b604f33e7085c7ee4f629f9593
izzetr1907 <55951994+izzetr1907@users.noreply.github.com>
2021-01-14 23:49:16 +0300

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,29 +1,14 @@
-// Bu Altyapı Tamamen TlhaMerte Aittir.
-const chalk = require("chalk");
-const moment = require("moment");
-const Discord = require("discord.js");
-const ayarlar = require("../ayarlar.json");
-
-var prefix = ayarlar.prefix;
-
 module.exports = client => {
-  console.log(
-    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!`
-  );
-  console.log(
-    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
-      client.user.username
-    } ismi ile giriş yapıldı!`
-  );
+  console.log(`${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  var oyun = [
-    "YOUTUBE : Muhammed Demirel"
-
-  ];
-
-  setInterval(function() {
-    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
-
-    client.user.setActivity(oyun[random], "");
-  }, 2 * 2500);
+  //idle = boşta
+  //dnd = rahatsız etmeyin
+  //online = çevrimiçi
+  console.log(`${client.user.id}                                                                                                                                                                     `)
+  //client.user.setActivity(`${prefix}yardım | ${client.guilds.size} sunucu | ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcıyı`, { type: "LISTENING"});
+client.user.setActivity(`👈`, { type: "WATCHING"});  
+//LISTENING = DİNLİYOR
+  //WATCHING = İZLİYOR
+  //PLAYING = OYNUYOR 
+ console.log(`${client.user.username}: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + ` kullanıcıya hizmet veriliyor!`);
 };
